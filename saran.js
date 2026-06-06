9f2edb470e7d50ee42fd3482112a6ee3c2b2a2f7
epsiepsiya07-boop <epsiepsiya07@gmail.com>
2026-06-06 19:39:09 +0530

    Add files via upload

---
+++ saran.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title");
+const button = document.getElementById("changebtn");
+
+button.addEventListener("click",function(){
+	title.textcontent = "Button Clicked!";
+	title.style.color="red";
+	
+	
+	
+});
